9f65eae9db1190ce5a16624aefd4d9fbbceec6ef
José Aguiar <zeca_henrique22@hotmail.com>
2021-02-10 21:41:38 -0300

    restante das alteracoes do ultimo commit
    restante das alteracoes do ultimo commit

--- ParkingProject/ParkingProject/Heranca_Pessoa/Pessoa.cs
+++ ParkingProject/ParkingProject/Heranca_Pessoa/Pessoa.cs
@@ -8,6 +8,8 @@ namespace ParkingProject.Heranca_Pessoa
 {
     public abstract class Pessoa
     {
+        public virtual string Usuario { get; set; }
+        public virtual string Senha { get; set; }
         public virtual string NivelPermissao { get; }
         public virtual string Nome { get; set; }
         public virtual int IdFuncionario { get; set; }
@@ -15,7 +17,9 @@ namespace ParkingProject.Heranca_Pessoa
         public virtual string RG { get; set; }
         public virtual DateTime DataDeNascimento { get; set; }
 
-        public Pessoa(string nome,int idFuncionario, string cpf,string rg,DateTime dataDeNascimento,string nivelPermissao) {
+        public Pessoa(string usuario,string senha,string nome,int idFuncionario, string cpf,string rg,DateTime dataDeNascimento,string nivelPermissao) {
+            this.Usuario = usuario;
+            this.Senha = senha;          
             this.Nome = nome;
             this.IdFuncionario = idFuncionario;
             this.CPF = cpf;
